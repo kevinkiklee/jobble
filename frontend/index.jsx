a24e6f45ed530aeb40e276c77d726a55362a7620
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-07 20:22:03 -0400

    Render React frontend

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,1 +1,7 @@
-document.write('HELLO');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootNode = document.getElementById('root');
+  ReactDOM.render(<h1>Jobble</h1>, rootNode);
+});
