8e4cc0d93d704deef58e83cc86162b4869d87b30
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-11 21:22:17 -0400

    Add skeleton links to Homepage and Main

--- frontend/layouts/Homepage/index.js
+++ frontend/layouts/Homepage/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 const Homepage = () => {
   return (
-    <div>
-      JOBBLE STATIC HOMEPAGE
+    <div className='homepage'>
+      <h1>JOBBLE STATIC HOMEPAGE</h1>
+      <Link to='/main' className='homepage__main-link'>
+        <h6>MAIN APP</h6>
+      </Link>
     </div>
   );
 };
--- frontend/layouts/Main/index.js
+++ frontend/layouts/Main/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 const Main = () => {
   return (
-    <div>
-      JOBBLE MAIN APPLICATION
+    <div className='main'>
+      <h1>JOBBLE MAIN APPLICATION</h1>
+      <Link to='/' className='main__homepage-link'>
+        <h6>HOMEPAGE LINK</h6>
+      </Link>
     </div>
   )
 };
