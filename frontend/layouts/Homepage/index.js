1d8e41d24cddf4e189fd6143f038aff03589d319
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-12 17:49:39 -0400

    Add login/logout buttons to Homepage

--- frontend/layouts/Homepage/index.js
+++ frontend/layouts/Homepage/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/authUtil';
 import './index.css';
 
-const Homepage = () => {
+const Homepage = (props) => {
+  const { isAuthenticated } = props.auth;
+  
   return (
     <div className='homepage'>
       <h1>JOBBLE STATIC HOMEPAGE</h1>
       <Link to='/main' className='homepage__main-link'>
         <h6>MAIN APP</h6>
       </Link>
+      {
+        !isAuthenticated() && (
+          <button
+            className='homepage__login-button'
+            onClick={() => props.auth.login()}
+          >
+            LOGIN
+          </button>
+        )
+      }
+      {
+        isAuthenticated() && (
+          <button
+            className='homepage__login-button'
+            onClick={() => props.auth.logout()}
+          >
+            LOGOUT
+          </button>
+        )
+      }
     </div>
   );
 };
