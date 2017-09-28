0d187f3313cf5a0fe244bff728532b007d2c55ab
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-28 17:31:14 -0400

    Lint utility files

--- frontend/utils/authUtil.js
+++ frontend/utils/authUtil.js
@@ -1,5 +1,5 @@
-import auth0 from 'auth0-js';
-import history from './history';
+import auth0 from 'auth0-js'
+import history from './history'
 
 export default class Auth {
   constructor() {
@@ -9,54 +9,53 @@ export default class Auth {
       redirectUri: 'http://jobble.herokuapp.com/callback',
       audience: 'https://kevinkiklee.auth0.com/userinfo',
       responseType: 'token id_token',
-      scope: 'openid'
+      scope: 'openid',
     }
 
     if (process.env.NODE_ENV !== 'production') {
       authSettings.redirectUri = 'http://localhost:3000/callback'
     }
 
-    this.auth0 = new auth0.WebAuth(authSettings);
-  
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.auth0 = new auth0.WebAuth(authSettings)
+
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
+    this.handleAuthentication = this.handleAuthentication.bind(this)
+    this.isAuthenticated = this.isAuthenticated.bind(this)
   }
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult);
-        history.replace('/main');
+        this.setSession(authResult)
+        history.replace('/main')
       } else if (err) {
-        history.replace('/');
-        console.log(err);
+        history.replace('/')
       }
-    });
+    })
   }
 
   setSession(authResult) {
-    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
-    history.replace('/main');
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime())
+    localStorage.setItem('access_token', authResult.accessToken)
+    localStorage.setItem('id_token', authResult.idToken)
+    localStorage.setItem('expires_at', expiresAt)
+    history.replace('/main')
   }
 
   login() {
-    this.auth0.authorize();
+    this.auth0.authorize()
   }
-  
+
   logout() {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
-    history.replace('/');
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('id_token')
+    localStorage.removeItem('expires_at')
+    history.replace('/')
   }
 
   isAuthenticated() {
-    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'))
+    return new Date().getTime() < expiresAt
   }
 }
