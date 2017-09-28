0b49f58d9ce62b03f95a7d4666b435c5e5bc3c2b
kevinkiklee <kevin.kik.lee@gmail.com>
2017-09-28 17:46:35 -0400

    Lint layout files

--- frontend/layouts/Root.js
+++ frontend/layouts/Root.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import history from '../utils/history';
-import App from './App';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Router } from 'react-router-dom'
+import history from '../utils/history'
+import App from './App'
 
-const Root = ({ store }) => {
-  return (
-    <Provider store={store}>
-      <Router history={history}>
-        <App />
-      </Router>
-    </Provider>
-  );
-};
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
+)
 
-export default Root;
+export default Root
